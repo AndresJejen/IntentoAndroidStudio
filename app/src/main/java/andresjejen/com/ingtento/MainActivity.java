729c63f5d0d52b7c036c9efd7eb4a0519ee961b0
Andres Jejen <gajcam@gmail.com>
2018-06-21 19:06:07 -0500

    Android Intento 1

--- app/src/main/java/andresjejen/com/ingtento/MainActivity.java
+++ app/src/main/java/andresjejen/com/ingtento/MainActivity.java
@@ -1,13 +1,34 @@
 package andresjejen.com.ingtento;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    Button Boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Referencia a elementos
+        Boton = (Button) findViewById(R.id.Btn1);
+
+        //Listener
+        Boton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent pantalla = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(pantalla);
+                finish();
+            }
+        });
     }
 }
+
+
